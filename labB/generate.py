d36414711a464dbbaf196e4145c7811a17bdf700
Anton Bengtsson <anton@antonbengtsson.net>
2018-04-25 10:18:17 +0200

    futhark-bench settings to process.fut and an updated generate.py

--- labB/generate.py
+++ labB/generate.py
@@ -4,15 +4,19 @@ import subprocess
 def main():
     sizes = [100, 1000, 10000, 100000, 1000000, 5000000, 10000000]
     bounds =  [-10000, 10000]
-    num_files = 2
+    num_files = 1
     for size in sizes:
         filename_base = "data-" + str(size) + "-"
         filename_suffix = ".dat"
         command = [ "futhark-dataset"
                   , "-b"
+                  , "--i32-bounds=" + str(bounds[0]) + ":" + str(bounds[1])
                   , "-g"
                   , "[" + str(size) + "]i32"
-                  , "--i32-bounds=" + str(bounds[0]) + ":" + str(bounds[1])]
+                  , "--i32-bounds=" + str(bounds[0]) + ":" + str(bounds[1])
+                  , "-g"
+                  , "[" + str(size) + "]i32"
+                  ]
 
         for i in range(num_files):
             file = open(filename_base + str(i) + filename_suffix, "w")
--- labB/process.fut
+++ labB/process.fut
@@ -1,3 +1,14 @@
+-- Given two signals, compute the maximum, pair-wise difference
+-- ==
+-- tags {100 1000 10000 100000 1000000 5000000 10000000}
+-- compiled input @data-100-0.dat
+-- compiled input @data-1000-0.dat
+-- compiled input @data-10000-0.dat
+-- compiled input @data-100000-0.dat
+-- compiled input @data-1000000-0.dat
+-- compiled input @data-5000000-0.dat
+-- compiled input @data-10000000-0.dat
+
 let max (x: i32) (y: i32) : i32 =
     if x > y then x else y
 
@@ -6,4 +17,4 @@ let diff (x: i32) (y: i32) : i32 = i32.abs(x - y)
 let process (x: []i32) (y: []i32) : i32 =
     reduce max 0 (map2 diff x y)
 
-let main (x: []i32) (y: []i32): i32 = process x y
+let main (x: []i32) (y: []i32) : i32 = process x y
